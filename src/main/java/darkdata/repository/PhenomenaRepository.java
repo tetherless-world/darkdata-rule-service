874f0c99bd7af3527dbab5a8699df3584237dbcd
zednis <lokipatera@gmail.com>
2015-10-16 13:36:28 -0600

    phenomena repository now returns OntClass objects.

--- src/main/java/darkdata/repository/PhenomenaRepository.java
+++ src/main/java/darkdata/repository/PhenomenaRepository.java
@@ -1,9 +1,18 @@
 package darkdata.repository;
 
+import darkdata.datasource.DarkDataDatasource;
 import darkdata.model.kb.Phenomena;
+import darkdata.model.ontology.DarkData;
+import org.apache.jena.ontology.Individual;
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author szednik
@@ -12,26 +21,55 @@ import java.util.*;
 @Service
 public class PhenomenaRepository {
 
-    // TODO replace with repository based on external datasource (file or endpoint)
+    @Autowired
+    private DarkDataDatasource datasource;
 
-    private static final Map<String,Phenomena> phenomenaMap;
-    static {
-        phenomenaMap = new HashMap<>();
-        phenomenaMap.put("hurricane", new Phenomena("hurricane", "Hurricane"));
-        phenomenaMap.put("volcanic_eruption", new Phenomena("volcanic_eruption", "Volcanic Eruption"));
-        phenomenaMap.put("dust_smoke_haze", new Phenomena("dust_smoke_haze","Dust, Smoke, Haze"));
-        phenomenaMap.put("flood", new Phenomena("flood", "Flood"));
-        phenomenaMap.put("drought", new Phenomena("drought", "Drought"));
-        phenomenaMap.put("earthquake", new Phenomena("earthquake", "Earthquake"));
-        phenomenaMap.put("wildfire", new Phenomena("wildfire", "Wildfire"));
-        phenomenaMap.put("severe_storm", new Phenomena("severe_storm", "Severe Storm"));
+    /*
+     * Returns a class with the given URI
+     * @param uri the URI of the class to get
+     * @return Optional object containing the OntClass (or empty if no class is found)
+     * @see OntClass
+     */
+    public Optional<OntClass> getClass(String uri) {
+        return Optional.ofNullable(datasource.getOntModel().getOntClass(uri));
     }
 
-    public Collection<Phenomena> list() {
-        return phenomenaMap.values();
+    /*
+     * Returns a dd:Phenomena subclass with the given label
+     * @param label the label of the class to get
+     * @return Optional object containing the OntClass (or empty if no class is found)
+     * @see OntClass
+     */
+    public Optional<OntClass> getClassByLabel(String label) {
+        return listSubclasses().stream()
+                .filter(c -> c.getLabel("en").equals(label))
+                .findAny();
     }
 
-    public Optional<Phenomena> get(String id) {
-        return Optional.ofNullable(phenomenaMap.get(id));
+    /*
+     * Returns a list of dd:Phenomena subclasses associated with the given topic
+     * @param topic EONET category name (or similar) associated with phenomena subclass
+     * @see OntClass
+     */
+    public List<OntClass> listClassesByTopic(String topic) {
+        return listSubclasses()
+                .stream()
+                .filter(c -> c.hasProperty(DarkData.topic, topic))
+                .collect(Collectors.toList());
+    }
+
+    /*
+     * Returns a list of subclasses of type dd:Phenomena
+     * @return List of OntClass objects for subclasses of dd:Phenomena
+     * @see OntClass
+     */
+    public List<OntClass> listSubclasses() {
+        return datasource.getOntModel()
+                .getOntClass(DarkData.Phenomena.getURI())
+                .listSubClasses()
+                .toList()
+                .stream()
+                .filter(c->!c.isAnon())
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/darkdata/repository/PhenomenaRepositoryTest.java
+++ src/test/java/darkdata/repository/PhenomenaRepositoryTest.java
@@ -2,6 +2,11 @@ package darkdata.repository;
 
 import darkdata.DarkDataApplication;
 import darkdata.model.kb.Phenomena;
+import darkdata.model.ontology.DarkData;
+import org.apache.jena.ontology.Individual;
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.ontology.OntResource;
+import org.apache.jena.rdf.model.Resource;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,8 +15,10 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * @author szednik
@@ -26,15 +33,32 @@ public class PhenomenaRepositoryTest {
     private PhenomenaRepository repository;
 
     @Test
-    public void testList() {
-        Collection<Phenomena> phenomenaCollection = repository.list();
-        Assert.assertFalse(phenomenaCollection.isEmpty());
+    public void testListSubclasses() {
+        List<OntClass> phenomenaList = repository.listSubclasses();
+        Assert.assertNotNull("list is null", phenomenaList);
+        Assert.assertFalse("list is empty", phenomenaList.isEmpty());
+        phenomenaList.stream().map(Resource::getURI).forEach(System.out::println);
     }
 
     @Test
-    public void testGet() {
-        Optional<Phenomena> phenomena = repository.get("hurricane");
-        Assert.assertTrue(phenomena.isPresent());
-        Assert.assertEquals("hurricane", phenomena.get().getId());
+    public void testGetClass() {
+        Optional<OntClass> hurricane = repository.getClass(DarkData.Hurricane.getURI());
+        Assert.assertTrue("class not found", hurricane.isPresent());
+        Assert.assertEquals("URIs do not match", DarkData.Hurricane.getURI(), hurricane.get().getURI());
+    }
+
+    @Test
+    public void testGetClassByLabel() {
+        Optional<OntClass> hurricane = repository.getClassByLabel("Hurricane");
+        Assert.assertTrue("class not found", hurricane.isPresent());
+        Assert.assertEquals("URIs do not match", DarkData.Hurricane.getURI(), hurricane.get().getURI());
+    }
+
+    @Test
+    public void testListClassesByTopic() {
+        List<OntClass> topicSevereStorms = repository.listClassesByTopic("Severe Storms");
+        Assert.assertNotNull("list is null", topicSevereStorms);
+        Assert.assertFalse("list is empty", topicSevereStorms.isEmpty());
+        topicSevereStorms.stream().map(Resource::getURI).forEach(System.out::println);
     }
 }
