e176fdd0dc90f6fc7c308c98ce63f3a53f31eabc
zednis <lokipatera@gmail.com>
2015-10-23 00:35:27 -0600

    refactored code

--- src/main/java/darkdata/service/SimpleScoringService.java
+++ src/main/java/darkdata/service/SimpleScoringService.java
@@ -17,27 +17,27 @@ public class SimpleScoringService implements ScoringService<CandidateWorkflowSco
 
     @Override
     public CandidateWorkflowScore score(CandidateWorkflow candidate) {
-
         List<CompatibilityAssertion> assertions = candidate.getCompatibilityAssertions();
         Double score = generateScore(assertions);
         String id = UUID.randomUUID().toString();
-
         return new CandidateWorkflowScore(id, candidate, score);
     }
 
     private Double generateScore(List<CompatibilityAssertion> assertions) {
-
-        Map<CompatibilityValue, List<CompatibilityAssertion>> groupedAssertions;
-        groupedAssertions = assertions.stream()
-                .collect(Collectors.groupingBy(a -> a.getValue().get()));
-
-        Map<CompatibilityValue, DoubleSummaryStatistics> statisticsMap =
-                groupedAssertions.entrySet().stream()
-                        .collect(Collectors.toMap(Map.Entry::getKey,
-                                e -> e.getValue().stream()
-                                        .map(a -> a.getConfidence().get())
-                                        .collect(Collectors.summarizingDouble(a -> a))));
-
+        Map<CompatibilityValue, DoubleSummaryStatistics> statisticsMap = getGroupedCompatibilitySummaries(assertions);
         return statisticsMap.entrySet().stream().findAny().map(a -> a.getValue().getAverage()).get();
+    }
+
+    private Map<CompatibilityValue, List<CompatibilityAssertion>> groupAssertions(List<CompatibilityAssertion> assertions) {
+        return assertions.stream()
+                .collect(Collectors.groupingBy(a -> a.getValue().get()));
+    }
+
+    private Map<CompatibilityValue, DoubleSummaryStatistics> getGroupedCompatibilitySummaries(List<CompatibilityAssertion> assertions) {
+        return groupAssertions(assertions).entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        e -> e.getValue().stream()
+                                .map(a -> a.getConfidence().get())
+                                .collect(Collectors.summarizingDouble(a -> a))));
     }
 }
