e5c17e7be8532446f669395c53feb3ebe7ff8433
zednis <lokipatera@gmail.com>
2015-10-23 13:03:22 -0600

    changed methodology used to compute candidate score

--- src/main/java/darkdata/service/SimpleScoringService.java
+++ src/main/java/darkdata/service/SimpleScoringService.java
@@ -6,7 +6,10 @@ import darkdata.model.kb.compatibility.CompatibilityAssertion;
 import darkdata.model.kb.compatibility.CompatibilityValue;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 /**
@@ -25,7 +28,9 @@ public class SimpleScoringService implements ScoringService<CandidateWorkflowSco
 
     private Double generateScore(List<CompatibilityAssertion> assertions) {
         Map<CompatibilityValue, DoubleSummaryStatistics> statisticsMap = getGroupedCompatibilitySummaries(assertions);
-        return statisticsMap.entrySet().stream().findAny().map(a -> a.getValue().getAverage()).get();
+        return statisticsMap.entrySet().stream()
+                .map(e -> getWeight(e.getKey()) * e.getValue().getAverage())
+                .collect(Collectors.summingDouble(v -> v));
     }
 
     private Map<CompatibilityValue, List<CompatibilityAssertion>> groupAssertions(List<CompatibilityAssertion> assertions) {
@@ -39,5 +44,29 @@ public class SimpleScoringService implements ScoringService<CandidateWorkflowSco
                         e -> e.getValue().stream()
                                 .map(a -> a.getConfidence().get())
                                 .collect(Collectors.summarizingDouble(a -> a))));
+    }
+
+    // TODO encode weights in properties file
+
+    private Double getWeight(CompatibilityValue value) {
+
+        if(value.equals(CompatibilityValue.NEGATIVE)) {
+            return -1d;
+        }
+        else if(value.equals(CompatibilityValue.INDIFFERENT)) {
+            return 1d;
+        }
+        else if(value.equals(CompatibilityValue.SLIGHT)) {
+            return 2d;
+        }
+        else if(value.equals(CompatibilityValue.SOME)) {
+            return 3d;
+        }
+        else if(value.equals(CompatibilityValue.STRONG)) {
+            return 5d;
+        }
+        else {
+            return 1d;
+        }
     }
 }
