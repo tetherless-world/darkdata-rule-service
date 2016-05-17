86df5b148bc228fc5199fa1d517d8cef24d2ca03
zednis <lokipatera@gmail.com>
2016-05-17 14:29:18 -0600

    refactored unit tests unit test resources

--- src/test/java/darkdata/service/RuleBasedCompatibilityServiceTest.java
+++ src/test/java/darkdata/service/RuleBasedCompatibilityServiceTest.java
@@ -13,7 +13,7 @@ import darkdata.repository.CandidateWorkflowRepository;
 import darkdata.repository.DataVariableRepository;
 import darkdata.repository.EventRepository;
 import darkdata.repository.G4ServiceRepository;
-import org.apache.jena.ontology.Individual;
+import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.rdf.model.InfModel;
 import org.junit.Assert;
@@ -56,26 +56,82 @@ public class RuleBasedCompatibilityServiceTest {
     @Autowired
     private RuleBasedReasoningService compatibilityRulesReasoningService;
 
-    @Test
-    public void testComputeCompatibility() {
+    private void testComputeCompatibilityOfEventType(String label, OntClass phenomenaClass) {
 
         OntModel m = datasource.createOntModel();
 
-        CandidateWorkflow candidate = repository.createCandidateWorkflow(m, "urn:candidate/rules/testComputeCompatibility").get();
-        Phenomena volcanicEruption = eventRepository.createEvent(m, "urn:event/testComputeCompatibility", DarkData.VolcanicEruption).get();
-        G4Service arAvTs = serviceRepository.getByIdentifier("ArAvTs").get();
+        CandidateWorkflow candidate = repository.createCandidateWorkflow(m, "urn:candidate/rules/testComputeCompatibility_"+label)
+                .orElseThrow((() -> new RuntimeException("Could not create candidate")));
+
+        Phenomena event = eventRepository.createEvent(m, "urn:event/testComputeCompatibility_"+label, phenomenaClass)
+                .orElseThrow(() -> new RuntimeException("Could not create event"));
+
+        G4Service arAvTs = serviceRepository.getByIdentifier("ArAvTs")
+                .orElseThrow(() -> new RuntimeException("Could not retrieve service"));
 
         Assert.assertEquals(m, candidate.getIndividual().getOntModel());
 
-        candidate.setEvent(volcanicEruption);
+        candidate.setEvent(event);
         candidate.setService(arAvTs);
 
-        List<PhysicalFeature> features = candidate.getEvent().get().getPhysicalFeatures();
-        Assert.assertNotNull("features is null", features);
+        m.prepare();
+
+        List<PhysicalFeature> features = candidate.getEvent()
+                .map(Phenomena::getPhysicalFeatures)
+                .orElseThrow(() -> new RuntimeException("Could not get event features"));
+
         Assert.assertFalse("features is empty", features.isEmpty());
 
-        // for test purposes this candidate will focus on the 1st feature inferred for the given event
-        candidate.setFeature(features.get(0));
+        PhysicalFeature feature = features.stream()
+                .filter(PhysicalFeature::hasConcreteFeatureClass)
+                .findFirst().orElseThrow(() -> new RuntimeException("could not find expected feature"));
+
+        candidate.setFeature(feature);
+
+        final InfModel inf = compatibilityRulesReasoningService.reason(m);
+
+        List<CompatibilityAssertion> assertions = service.computeCompatibilities(inf, candidate);
+        Assert.assertFalse(assertions.isEmpty());
+
+        assertions.stream()
+                .forEach(a -> Assert.assertTrue(a.getValue().isPresent()));
+    }
+
+    private void testComputeCompatibilityOfEventType(String label, OntClass phenomenaClass, String datafieldId) {
+
+        OntModel m = datasource.createOntModel();
+
+        CandidateWorkflow candidate = repository.createCandidateWorkflow(m, "urn:candidate/rules/testComputeCompatibility_"+label)
+                .orElseThrow((() -> new RuntimeException("Could not create candidate")));
+
+        Phenomena event = eventRepository.createEvent(m, "urn:event/testComputeCompatibility_"+label, phenomenaClass)
+                .orElseThrow(() -> new RuntimeException("Could not create event"));
+
+        G4Service arAvTs = serviceRepository.getByIdentifier("ArAvTs")
+                .orElseThrow(() -> new RuntimeException("Could not retrieve service"));
+
+        DataVariable variable = variableRepository.getByIdentifier(datafieldId)
+                .orElseThrow(() -> new RuntimeException("could not retrieve datafield"));
+
+        Assert.assertEquals(m, candidate.getIndividual().getOntModel());
+
+        candidate.setEvent(event);
+        candidate.setService(arAvTs);
+        candidate.addVariable(variable);
+
+        m.prepare();
+
+        List<PhysicalFeature> features = candidate.getEvent()
+                .map(Phenomena::getPhysicalFeatures)
+                .orElseThrow(() -> new RuntimeException("Could not get event features"));
+
+        Assert.assertFalse("features is empty", features.isEmpty());
+
+        PhysicalFeature feature = features.stream()
+                .filter(PhysicalFeature::hasConcreteFeatureClass)
+                .findFirst().orElseThrow(() -> new RuntimeException("could not find expected feature"));
+
+        candidate.setFeature(feature);
 
         final InfModel inf = compatibilityRulesReasoningService.reason(m);
 
@@ -88,118 +144,42 @@ public class RuleBasedCompatibilityServiceTest {
 
     @Test
     public void testComputeCompatibility_SevereStorm() {
-
-        OntModel m = datasource.createOntModel();
-
-        CandidateWorkflow candidate = repository.createCandidateWorkflow(m, "urn:candidate/rules/testComputeCompatibility_SevereStorm").get();
-        Phenomena volcanicEruption = eventRepository.createEvent(m, "urn:event/testComputeCompatibility", DarkData.SevereStorm).get();
-        G4Service arAvTs = serviceRepository.getByIdentifier("ArAvTs").get();
-
-        Assert.assertEquals(m, candidate.getIndividual().getOntModel());
-
-        candidate.setEvent(volcanicEruption);
-        candidate.setService(arAvTs);
-
-        List<PhysicalFeature> features = candidate.getEvent().get().getPhysicalFeatures();
-        Assert.assertNotNull("features is null", features);
-        Assert.assertFalse("features is empty", features.isEmpty());
-
-        // for test purposes this candidate will focus on the 1st feature inferred for the given event
-        candidate.setFeature(features.get(0));
-
-        final InfModel inf = compatibilityRulesReasoningService.reason(m);
-
-        List<CompatibilityAssertion> assertions = service.computeCompatibilities(inf, candidate);
-        Assert.assertFalse(assertions.isEmpty());
-
-        assertions.stream()
-                //.forEach(a -> Assert.assertTrue(a.getValue().isPresent()));
-                .forEach(a -> System.out.println(a.getValue().get().getIdentifier().get()));
+        testComputeCompatibilityOfEventType("SevereStorm", DarkData.SevereStorm);
     }
+
+    @Test
+    public void testComputeCompatibility_Wildfire() {
+        testComputeCompatibilityOfEventType("Wildfire", DarkData.Wildfire);
+    }
+
+    @Test
+    public void testComputeCompatibility_VolcanicEruption() {
+        testComputeCompatibilityOfEventType("VolcanicEruption", DarkData.VolcanicEruption);
+    }
+
+    @Test
+    public void testComputeCompatibility_DustAndHaze() {
+        testComputeCompatibilityOfEventType("DustAndHaze", DarkData.DustAndHaze);
+    }
+
+    // TODO add compatibility rules to catch basic drought with no variables
+//    @Test
+//    public void testComputeCompatibility_Drought() {
+//        testComputeCompatibilityOfEventType("Drought", DarkData.Drought);
+//    }
 
     @Test
     public void testComputeCompatibility_Hurricane() {
-
-        OntModel m = datasource.createOntModel();
-
-        CandidateWorkflow candidate = repository.createCandidateWorkflow(m, "urn:candidate/rules/testComputeCompatibility_Hurricane")
-                .orElseThrow(() -> new RuntimeException("could not create candidate workflow"));
-
-        Phenomena hurricane = eventRepository.createEvent(m, "urn:event/testComputeCompatibility_Hurricane", DarkData.Hurricane)
-                .orElseThrow(() -> new RuntimeException("could not create event"));
-
-        G4Service arAvTs = serviceRepository.getByIdentifier("ArAvTs")
-                .orElseThrow(() -> new RuntimeException("could not create service"));
-
-        DataVariable variable = variableRepository.createDataVariable(m, "urn:variable/testComputeCompatibility_Hurricane")
-                .orElseThrow(() -> new RuntimeException("could not create data variable"));
-
-        Individual halfHourly = m.createIndividual("http://darkdata.tw.rpi.edu/data/time-interval/half-hourly", DarkData.TimeInterval);
-        variable.getIndividual().addProperty(DarkData.timeInterval, halfHourly);
-
-        Assert.assertEquals(m, candidate.getIndividual().getOntModel());
-
-        candidate.setEvent(hurricane);
-        candidate.setService(arAvTs);
-        candidate.addVariable(variable);
-
-        List<PhysicalFeature> features = candidate.getEvent()
-                .orElseThrow(() -> new RuntimeException("could not get candidate event"))
-                .getPhysicalFeatures();
-
-        Assert.assertFalse("features is empty", features.isEmpty());
-
-        // for test purposes this candidate will focus on only the windfields feature
-        PhysicalFeature windFields = features.stream()
-                .filter(f -> f.hasType(DarkData.WindFields))
-                .findFirst().orElseThrow(() -> new RuntimeException("could not find expected feature"));
-
-        candidate.setFeature(windFields);
-
-        final InfModel inf = compatibilityRulesReasoningService.reason(m);
-        List<CompatibilityAssertion> assertions = service.computeCompatibilities(inf, candidate);
-        Assert.assertFalse(assertions.isEmpty());
+        testComputeCompatibilityOfEventType("Hurricane", DarkData.Hurricane);
     }
 
     @Test
-    public void testComputeCompatibility_Hurricane_2() {
-
-        OntModel m = datasource.createOntModel();
-
-        CandidateWorkflow candidate = repository.createCandidateWorkflow(m, "urn:candidate/rules/testComputeCompatibility_Hurricane")
-                .orElseThrow(() -> new RuntimeException("could not create candidate workflow"));
-
-        Phenomena hurricane = eventRepository.createEvent(m, "urn:event/testComputeCompatibility_Hurricane", DarkData.Hurricane)
-                .orElseThrow(() -> new RuntimeException("could not create event"));
-
-        G4Service arAvTs = serviceRepository.getByIdentifier("ArAvTs")
-                .orElseThrow(() -> new RuntimeException("could not retrieve service"));
-
-        DataVariable variable = variableRepository.getByIdentifier("MAT1NXSLV_5_2_0_UV10M_mag")
-                .orElseThrow(() -> new RuntimeException("could not retrieve datafield"));
-
-        Assert.assertEquals(m, candidate.getIndividual().getOntModel());
-
-        candidate.setEvent(hurricane);
-        candidate.setService(arAvTs);
-        candidate.addVariable(variable);
-
-        List<PhysicalFeature> features = candidate.getEvent()
-                .orElseThrow(() -> new RuntimeException("could not get candidate event"))
-                .getPhysicalFeatures();
-
-        Assert.assertFalse("features is empty", features.isEmpty());
-
-        // for test purposes this candidate will focus on only the windfields feature
-        PhysicalFeature windFields = features.stream()
-                .filter(f -> f.hasType(DarkData.WindFields))
-                .findFirst().orElseThrow(() -> new RuntimeException("could not find expected feature"));
-
-        candidate.setFeature(windFields);
-
-        final InfModel inf = compatibilityRulesReasoningService.reason(m);
-        List<CompatibilityAssertion> assertions = service.computeCompatibilities(inf, candidate);
-        Assert.assertFalse(assertions.isEmpty());
+    public void testComputeCompatibility_Hurricane_and_variable() {
+        testComputeCompatibilityOfEventType("Hurricane_and_variable", DarkData.Hurricane, "MAT1NXSLV_5_2_0_UV10M_mag");
     }
 
+    @Test
+    public void name() throws Exception {
+
+    }
 }
